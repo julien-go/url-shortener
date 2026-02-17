99e9cdc90b3c5d7d55c2c1d67299bc4583c37da0
julien-go <juliengodard97@gmail.com>
2026-02-17 22:56:22 +0100

    fix eslint.config backend

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -1,29 +1,15 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
 
-export default defineConfig([
-  globalIgnores(["dist"]),
-  globalIgnores(["dist", "node_modules"]),
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ["./tsconfig.app.json", "./tsconfig.node.json"],
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
+    ignores: ["dist/**", "node_modules/**"],
+  },
+
+  {
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "error",
@@ -32,4 +18,4 @@ export default defineConfig([
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
-]);
+];
